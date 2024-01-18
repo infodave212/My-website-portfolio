f5af55d5e19e943dbe653fa51396a5d7d5b0cf67
Dave O <infodave212@gmail.com>
2024-01-17 21:54:13 -0600

    adding all of the file

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const navLinks = document.querySelectorAll('nav a');
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function(event) {
+            event.preventDefault();
+            const targetSectionId = link.getAttribute('href').substring(1);
+            const targetSection = document.getElementById(targetSectionId);
+
+            if (targetSection) {
+                window.scrollTo({
+                    top: targetSection.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
+    });
+});
